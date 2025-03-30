8b4f2208c9aa1bd282ad67b94c2c14c867b6ea7e
ralph1233 <ralphmerhy93@gmail.com>
2025-03-30 13:15:01 +0300

    tp1 ex1

--- tp1/ex1/app/src/main/java/com/example/ex1/MainActivity.kt
+++ tp1/ex1/app/src/main/java/com/example/ex1/MainActivity.kt
@@ -1,20 +1,37 @@
 package com.example.ex1
 
-import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import android.os.Bundle
+import android.widget.Button
+import android.widget.RadioButton
+import android.widget.RadioGroup
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var radioGroup: RadioGroup
+    private lateinit var showButton: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        radioGroup = findViewById(R.id.radioGroupLanguage)
+        showButton = findViewById(R.id.showButton)
+
+        showButton.setOnClickListener {
+            val selectedId = radioGroup.checkedRadioButtonId
+
+            if (selectedId == -1) {
+                Toast.makeText(this, "Please select a language", Toast.LENGTH_SHORT).show()
+            } else {
+                val selectedRadioButton = findViewById<RadioButton>(selectedId)
+                val language = if (selectedRadioButton.id == R.id.radioButtonFrench) "French" else "English"
+
+                val intent = Intent(this, ListActivity::class.java)
+                intent.putExtra("language", language)
+                startActivity(intent)
+            }
         }
     }
 }
