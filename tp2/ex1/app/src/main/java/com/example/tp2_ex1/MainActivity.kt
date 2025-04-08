681a8ef67ac7e15926a7fa1f66b72e305defe9ba
ralph1233 <ralphmerhy93@gmail.com>
2025-04-08 03:23:10 +0300

    tp2 ex1 finished

--- tp2/ex1/app/src/main/java/com/example/tp2_ex1/MainActivity.kt
+++ tp2/ex1/app/src/main/java/com/example/tp2_ex1/MainActivity.kt
@@ -1,20 +1,39 @@
 package com.example.tp2_ex1
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var buttonFragment1: Button
+    private lateinit var buttonFragment2: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        // Initialize buttons using findViewById
+        buttonFragment1 = findViewById(R.id.buttonFragment1)
+        buttonFragment2 = findViewById(R.id.buttonFragment2)
+
+        // Load default fragment
+        replaceFragment(FragmentOne())
+
+        // Button click listeners
+        buttonFragment1.setOnClickListener {
+            replaceFragment(FragmentOne())
         }
+
+        buttonFragment2.setOnClickListener {
+            replaceFragment(FragmentTwo())
+        }
+    }
+
+    private fun replaceFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragmentContainer, fragment)
+            .commit()
     }
 }
